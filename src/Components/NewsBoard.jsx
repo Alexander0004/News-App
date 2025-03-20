13c52cbbe41a906541bd01b948294235a7650e8a
Alexander0004 <allan_alex1@outlook.com>
2025-03-20 11:39:54 +0530

    second commit

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,33 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import NewsItem from './NewsItem'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NewsItem from './NewsItem';
 
-const NewsBoard = ({category}) => {
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
 
-    const [articles, setArticles] = useState([])
-
-    useEffect(()=>{
-        fetchArticle()
-    }, [category])
-   const fetchArticle = async ()=>{
-    const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&${category}&apiKey=5eb1bbaff87b4ba1afabab37c600266e`)
-    console.log(res)
-    setArticles(res.data.articles)
-   }
+  useEffect(() => {
+    fetchArticle();
+  }, [category]);
+  const fetchArticle = async () => {
+    const res = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us&${category}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    console.log(res);
+    setArticles(res.data.articles);
+  };
   return (
-    <><h2 className='text-center'><span>Trending News</span></h2>
+    <>
+      <h2 className="text-center">
+        <span>Trending News</span>
+      </h2>
 
-    {articles && (
-        articles.map((news, index)=>
-         <NewsItem key={index} title={news.title} description={news.description} url={news.url} urlToImage={news.urlToImage} author={news.author}/>
-        )
-    )}
-    
-    
+      {articles &&
+        articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            url={news.url}
+            urlToImage={news.urlToImage}
+            author={news.author}
+          />
+        ))}
     </>
+  );
+};
 
-    
-  )
-}
-
-export default NewsBoard
+export default NewsBoard;
